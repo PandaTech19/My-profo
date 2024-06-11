9f91d52b2ea21fa52b3453b788566f084299332e
Grimhunter <pandatech1999@gmail.com>
2024-06-11 11:56:57 +0300

    added Orbg and changed the ranks of work

--- src/Mycontent/My-work/work.js
+++ src/Mycontent/My-work/work.js
@@ -11,8 +11,8 @@ import Camera from "./Camera.gif";
 import Photography from "./thumbnail.png";
 import Stocks from "./stocks.jpg";
 import Crypto from "./crypto.gif";
-import Old_pf from "./old-prof.jpg";
-import Old from "./old-pf.gif";
+import Orb from "./orbg.png"
+import Orbg from "./house.gif"
 
 export default function Work() {
     const { t, isArabic, theme, Light } = useContext(AppContext);
@@ -47,7 +47,88 @@ export default function Work() {
                     </span>
                 </p>
             </div>
+            {/* Orbg Project */}
+            <>
+                <div className="Work__proj-box">
+                    <div className="Work__proj-pic-container" data-aos="fade-right" data-aos-duration="2000">
+                        <a className="Work__proj-link" href="https://www.orb.bg" target="__blank">
+                            <img
+                                src={Orb}
+                                alt="The project to drone store"
+                                className="Work__proj-pic"
+                            />
+                        </a>
+                    </div>
+                    <div className={`Work__info-container ${isArabic ? "Work__info-container--arb" : ""}`}
+                        data-aos="fade-left"
+                        data-aos-duration="2000"
+                    >                        <div className="Work__proj-title-container">
+                            <h1 className="Work__proj-title">{Name[5]}</h1>
+                            <img src={Orbg} alt="A floating drone" className="Work__proj-gif" />
+                        </div>
+                        <p style={{ color: theme ? `${Light}` : "" }}
+                            className={`Work__proj-info ${isArabic ? "Work__proj-info--arb" : ""}`}>
+                            {Info[5]}
+                        </p>
 
+                    </div>
+                </div>
+            </>
+            {/* Photography Project */}
+            <>
+                <div className="Work__proj-box">
+                    <div className="Work__proj-pic-container" data-aos="fade-right" data-aos-duration="2000">
+                        <a className="Work__proj-link" href="https://pick-best-moments.vercel.app" target="__blank">
+                            <img
+                                src={Photography}
+                                alt="The project to Photography"
+                                className="Work__proj-pic"
+                            />
+                        </a>
+                    </div>
+                    <div className={`Work__info-container ${isArabic ? "Work__info-container--arb" : ""}`}
+                        data-aos="fade-left"
+                        data-aos-duration="2000"
+                    >                        <div className="Work__proj-title-container">
+                            <h1 className="Work__proj-title">{Name[3]}</h1>
+                            <img src={Camera} alt="A Clicking camera" className="Work__proj-gif-camera" />
+                        </div>
+                        <p
+                            style={{ color: theme ? `${Light}` : "" }}
+                            className={`Work__proj-info ${isArabic ? "Work__proj-info--arb" : ""}`}>
+                            {Info[3]}
+                        </p>
+                    </div>
+                </div>
+            </>
+            {/* Stocks Project */}
+            <>
+                <div className="Work__proj-box">
+                    <div className="Work__proj-pic-container" data-aos="fade-right" data-aos-duration="2000">
+                        <a className="Work__proj-link" href="https://stocks-azure.vercel.app" target="__blank">
+                            <img
+                                src={Stocks}
+                                alt="The project to drone store"
+                                className="Work__proj-pic"
+                            />
+                        </a>
+                    </div>
+                    <div className={`Work__info-container ${isArabic ? "Work__info-container--arb" : ""}`}
+                        data-aos="fade-left"
+                        data-aos-duration="2000"
+                    >                        <div className="Work__proj-title-container">
+                            <h1 className="Work__proj-title">{Name[4]}</h1>
+                            <img src={Crypto} alt="A floating drone" className="Work__proj-gif" />
+                        </div>
+                        <p style={{ color: theme ? `${Light}` : "" }}
+                            className={`Work__proj-info ${isArabic ? "Work__proj-info--arb" : ""}`}>
+                            {Info[4]}
+                        </p>
+
+                    </div>
+                </div>
+            </>
+            {/* Drone Project */}
             <>
                 <div className="Work__proj-box">
                     <div className="Work__proj-pic-container" data-aos="fade-right" data-aos-duration="2000">
@@ -75,7 +156,7 @@ export default function Work() {
                     </div>
                 </div>
             </>
-
+            {/* Study In UA Project */}
             <>
                 <div className="Work__proj-box">
                     <div className="Work__proj-pic-container" data-aos="fade-right" data-aos-duration="2000">
@@ -102,7 +183,7 @@ export default function Work() {
                     </div>
                 </div>
             </>
-
+            {/* Movie Picker project  */}
             <>
                 <div className="Work__proj-box">
                     <div className="Work__proj-pic-container" data-aos="fade-right" data-aos-duration="2000">
@@ -126,88 +207,6 @@ export default function Work() {
                             className={`Work__proj-info ${isArabic ? "Work__proj-info--arb" : ""}`}>
                             {Info[2]}
                         </p>
-
-                    </div>
-                </div>
-            </>
-
-            <>
-                <div className="Work__proj-box">
-                    <div className="Work__proj-pic-container" data-aos="fade-right" data-aos-duration="2000">
-                        <a className="Work__proj-link" href="https://pick-best-moments.vercel.app" target="__blank">
-                            <img
-                                src={Photography}
-                                alt="The project to Photography"
-                                className="Work__proj-pic"
-                            />
-                        </a>
-                    </div>
-                    <div className={`Work__info-container ${isArabic ? "Work__info-container--arb" : ""}`}
-                        data-aos="fade-left"
-                        data-aos-duration="2000"
-                    >                        <div className="Work__proj-title-container">
-                            <h1 className="Work__proj-title">{Name[3]}</h1>
-                            <img src={Camera} alt="A Clicking camera" className="Work__proj-gif-camera" />
-                        </div>
-                        <p
-                            style={{ color: theme ? `${Light}` : "" }}
-                            className={`Work__proj-info ${isArabic ? "Work__proj-info--arb" : ""}`}>
-                            {Info[3]}
-                        </p>
-                    </div>
-                </div>
-            </>
-
-            <>
-                <div className="Work__proj-box">
-                    <div className="Work__proj-pic-container" data-aos="fade-right" data-aos-duration="2000">
-                        <a className="Work__proj-link" href="https://stocks-azure.vercel.app" target="__blank">
-                            <img
-                                src={Stocks}
-                                alt="The project to drone store"
-                                className="Work__proj-pic"
-                            />
-                        </a>
-                    </div>
-                    <div className={`Work__info-container ${isArabic ? "Work__info-container--arb" : ""}`}
-                        data-aos="fade-left"
-                        data-aos-duration="2000"
-                    >                        <div className="Work__proj-title-container">
-                            <h1 className="Work__proj-title">{Name[4]}</h1>
-                            <img src={Crypto} alt="A floating drone" className="Work__proj-gif" />
-                        </div>
-                        <p style={{ color: theme ? `${Light}` : "" }}
-                            className={`Work__proj-info ${isArabic ? "Work__proj-info--arb" : ""}`}>
-                            {Info[4]}
-                        </p>
-
-                    </div>
-                </div>
-            </>
-
-            <>
-                <div className="Work__proj-box">
-                    <div className="Work__proj-pic-container" data-aos="fade-right" data-aos-duration="2000">
-                        <a className="Work__proj-link" href="https://my-protfolio-me.vercel.app" target="__blank">
-                            <img
-                                src={Old_pf}
-                                alt="The project to drone store"
-                                className="Work__proj-pic"
-                            />
-                        </a>
-                    </div>
-                    <div className={`Work__info-container ${isArabic ? "Work__info-container--arb" : ""}`}
-                        data-aos="fade-left"
-                        data-aos-duration="2000"
-                    >                        <div className="Work__proj-title-container">
-                            <h1 className="Work__proj-title">{Name[5]}</h1>
-                            <img src={Old} alt="A floating drone" className="Work__proj-gif" />
-                        </div>
-                        <p style={{ color: theme ? `${Light}` : "" }}
-                            className={`Work__proj-info ${isArabic ? "Work__proj-info--arb" : ""}`}>
-                            {Info[5]}
-                        </p>
-
                     </div>
                 </div>
             </>
